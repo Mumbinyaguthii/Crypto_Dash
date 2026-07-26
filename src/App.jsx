a6baf48f5c14f2f22f441ad13ef89831b6fac1b0
mumbinyaguthii <janenyaguthii931@gmail.com>
2026-07-27 00:32:07 +0300

    added not found and header files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router";
+import Header from "./components/Header";
 import HomePage from "./pages/home";
 import AboutPage from "./pages/about";
+import NotFoundPage from "./pages/not-found";
+import CoinDetailsPage from "./pages/coin-details";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -51,25 +54,30 @@ const App = () => {
   }, [limit]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <HomePage
-            coins={coins}
-            filter={filter}
-            setFilter={setFilter}
-            limit={limit}
-            setLimit={setLimit}
-            sortBy={sortBy}
-            setSortBy={setSortBy}
-            loading={loading}
-            error={error}
-          />
-        }
-      />
-      <Route />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              filter={filter}
+              setFilter={setFilter}
+              limit={limit}
+              setLimit={setLimit}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              loading={loading}
+              error={error}
+            />
+          }
+        />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/coin/:id" element={<CoinDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
 
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,10 @@ const AboutPage = () => {
     <div className="about">
       <h1>About Crypto Dash</h1>
       <p>
+        Crypto Dash is a simple React Application that displays live
+        cryptocurrency data using the CoinGecko API.
+      </p>
+      <p>
         You can explore top cryptocurries by market cap, filter by name or
         symbol, and then by price, market cap, or 24-hour change.
       </p>
@@ -12,7 +16,7 @@ const AboutPage = () => {
       </p>
       <p>
         🚀Future features might include detailed coin views, favourites,
-        pagination , and much more!
+        pagination, and much more!
       </p>
     </div>
   );
